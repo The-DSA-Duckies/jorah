bec77e0ad5a683f41d26881411af025a3ba950f8
benny <bennycortese@gmail.com>
2024-04-04 01:43:32 -0400

    Mapping now works based on TA split, keeping that file hidden so we don't have students complaining (though it doesn't really matter at this point)

--- api/ml_model_schenanigans/label_maker.py
+++ api/ml_model_schenanigans/label_maker.py
@@ -1,7 +1,23 @@
+def load_ta_student_pairs(filename):
+    ta_student_pairs = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            ta, students = line.strip().split(': ')
+            students_list = students.split(', ')
+            ta_student_pairs[ta] = students_list
+    return ta_student_pairs
+
+ta_student_pairs = load_ta_student_pairs('ta_student_mapping.txt')
 
 
+ta_range_pairs = {ta: (students[0], students[-1]) for ta, students in ta_student_pairs.items()}
 
+def which_ta(student_name):
+    for ta, (start, end) in ta_range_pairs.items():
+        if start <= student_name <= end:
+            return ta
+    return "Student's TA could not be determined based on the provided names."
 
-for student in students:
+    
 
-label_each_student(TA_DATA)
+print(which_ta("Bill Nye"))
